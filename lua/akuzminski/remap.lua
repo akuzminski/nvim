5dbc1377945b94e9f1a53ed3262bec57bfc15185
akuzminski <Antonio.Kuzminski@logicbricks.com>
2024-04-16 15:31:31 +0200

    Added new key bindings and formatting on save

--- lua/akuzminski/remap.lua
+++ lua/akuzminski/remap.lua
@@ -1,2 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+    end,
+})
