e9641b6a8de26c7f1bd22b9a8cd6fd97fc9ff497
akuzminski <Antonio.Kuzminski@logicbricks.com>
2024-04-16 15:34:37 +0200

    Lsp config file

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,26 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+	lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-space>'] = cmp.mapping.complete(),
+    }),
+})
+
+require('mason').setup({})
+require('mason-lspconfig').setup({})
+require'lspconfig'.pyright.setup{}
+
